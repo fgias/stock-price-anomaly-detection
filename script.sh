278842c4fc718a472afd84a7bbbb9134b6c1791c
Fotis Giasemis <fotis@fotisgiasemis.com>
2026-02-18 01:13:48 +0200

    Save training log and plot it

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
 cd stgan-algo/STGAN
-mamba run -n stgan python main.py --dataset crypto --lambda_G 1 --trend_time 7 > training.log 2>&1
+mamba run -n stgan \
+    python main.py \
+    --dataset crypto \
+    --lambda_G 1 \
+    --trend_time 7 \
+    --epoch 10 \
+    > crypto/result/training.log 2>&1
