262ceda0e892889ece34b042995663802341649b
Fotis Giasemis <fotis@fotisgiasemis.com>
2026-02-19 01:27:18 +0200

    Run crypto volume training

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 cd stgan/STGAN
 mamba run -n stgan \
     python main.py \
-    --dataset crypto \
-    --lambda_G 1 \
+    --dataset crypto_volume \
+    --lambda_G 10 \
     --trend_time 7 \
     --epoch 20 \
-    2>&1 | tee crypto/result/training.log
+    2>&1 | tee crypto_volume/result/training.log
